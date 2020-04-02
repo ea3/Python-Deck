8c233b96a645e385e8d918feaeea85c51a0ca81d
Emilio Araos <ea3dev@gmail.com>
2020-04-02 02:06:30 -0600

    sort the deck

--- cardDeck.py
+++ cardDeck.py
@@ -1,4 +1,5 @@
 import collections
+from random import choice
 
 # collections.namedtuple(typename, field_names[, verbose=False][, rename=False])
 Card = collections.namedtuple('Card', ['rank', 'suit'])
@@ -21,26 +22,59 @@ class FrenchDeck:
 
 
 deck = FrenchDeck()
+# Picking only the aces by slicing, starting at index 12 and skipping 13 cards.
+print(deck[12::13])
+
+# How many cards? Use len() on the instance.
 print(len(deck))
+# random card? Use choice on the instance. Remember to import Random.
+print(choice(deck))
+# The First three elements.
+print(deck[:3])
+# for card in reversed(deck):
+#     print(card)
+# Reversed the cards. Two different methods.
+# for card in deck[::-1]:
+#     print(card)
+print(Card('Q', 'hearts') in deck)
+# Ranking cards: Ace is the highest, Spades, hearts, diamonds, clubs(lowest)
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+print(type(suit_values))
+print(suit_values)
 
-for rank in FrenchDeck.ranks:
-    print(rank)
 
-for suit in FrenchDeck.suits:
-    print(suit)
-
-
-print(FrenchDeck.ranks)
-print(FrenchDeck.suits)
-capital = [(suit.capitalize(), rank.capitalize()) for suit in FrenchDeck.suits for rank in FrenchDeck.ranks]
-print(capital)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
 
 
-cartas = [Card(rank, suit) for suit in deck.suits
-          for rank in deck.ranks]
-print(cartas)
-print(cartas[-1])
+for card in sorted(deck, key=spades_high):
+    print(card)
 
+
+
+
+
+
+
+# for card in deck:
+#     print(card)
+
+# for suit in FrenchDeck.suits:
+#     print(suit)
+#
+#
+# print(FrenchDeck.ranks)
+# print(FrenchDeck.suits)
+# capital = [(suit.capitalize(), rank.capitalize()) for suit in FrenchDeck.suits for rank in FrenchDeck.ranks]
+# print(capital)
+#
+#
+# cartas = [Card(rank, suit) for suit in deck.suits
+#           for rank in deck.ranks]
+# print(cartas)
+# print(cartas[-1])
+#
 
 
 
